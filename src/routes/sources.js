469250777be85eb4ff8f77c70fd3f9e1104dd604
Efthymios Grigorakis <73905568+eegrigor@users.noreply.github.com>
2023-01-19 22:08:16 +0200

    Comments added

--- src/routes/sources.js
+++ src/routes/sources.js
@@ -7,6 +7,7 @@ const router = express.Router();
 
 const Source = require('../models/source');
 
+// Gets all the sources of a user 
 router.get('/sources',
   authorization,
   async (req, res, next) => {
@@ -14,6 +15,7 @@ router.get('/sources',
       const {id} = req.decoded;
       const foundSources = await Source.find({owner: mongoose.Types.ObjectId(id)});
       const sources = [];
+      //getting the sources 
       foundSources.forEach((s) => {
         sources.push({
           id: s._id,
@@ -26,7 +28,8 @@ router.get('/sources',
           active: false
         });
       });
-
+      
+      //return sources 
       return res.json({
         success: true,
         sources
@@ -36,19 +39,24 @@ router.get('/sources',
     }
   });
 
+  // creating a source
 router.post('/create-source', 
   authorization,
   async (req, res, next) => {
     try {
+      //giving name, type, url, login, passcode and vhost for the new source
       const {name, type, url, login, passcode, vhost} = req.body;
       const {id} = req.decoded;
+      // checking if a host with the same name exists 
       const foundSource = await Source.findOne({owner: mongoose.Types.ObjectId(id), name});
       if (foundSource) {
+        //return message and error code 
         return res.json({
           status: 409,
           message: 'A source with that name already exists.'
         });
       }
+      //creating the new source
       await new Source({
         name,
         type,
@@ -65,13 +73,17 @@ router.post('/create-source',
     }
   }); 
 
+  // changing an existing source
 router.post('/change-source', 
   authorization,
   async (req, res, next) => {
     try {
+      // giving the new properties of the souce
       const {id, name, type, url, login, passcode, vhost} = req.body;
       const foundSource = await Source.findOne({_id: mongoose.Types.ObjectId(id), owner: mongoose.Types.ObjectId(req.decoded.id)});
       if (!foundSource) {
+        // checking if a host with the given id exists 
+        // if it does not exist return error code and a message 
         return res.json({
           status: 409,
           message: 'The selected source has not been found.'
@@ -80,12 +92,15 @@ router.post('/change-source',
       
       const sameNameSources = await Source.findOne({_id: {$ne: mongoose.Types.ObjectId(id)}, owner: mongoose.Types.ObjectId(req.decoded.id), name});
       if (sameNameSources) {
+        //checking if another source with the same name exists
+        //if another source has the same name return error code and a message 
         return res.json({
           status: 409,
           message: 'A source with the same name has been found.'
         });
       }
-
+      
+      // giving the source the new properties 
       foundSource.name = name;
       foundSource.type = type;
       foundSource.url = url;
@@ -100,14 +115,18 @@ router.post('/change-source',
     }
   }); 
 
+  // delete a source
 router.post('/delete-source', 
   authorization,
   async (req, res, next) => {
     try {
+      //  give the id of the source to be deleted 
       const {id} = req.body;
 
       const foundSource = await Source.findOneAndRemove({_id: mongoose.Types.ObjectId(id), owner: mongoose.Types.ObjectId(req.decoded.id)});
       if (!foundSource) {
+        //checking if a source with the given id exists
+        //if it does not exist return error code and a message 
         return res.json({
           status: 409,
           message: 'The selected source has not been found.'
@@ -119,19 +138,24 @@ router.post('/delete-source',
     }
   }); 
 
+  // finding a specific source
 router.post('/source',
   async (req, res, next) => {
     try {
+      // give the name of the source, the owner and the user
       const {name, owner, user} = req.body;
       const userId = (owner === 'self') ? user.id : owner;
       const foundSource = await Source.findOne({name, owner: mongoose.Types.ObjectId(userId)});
       if (!foundSource) {
+        // checking if the wanted source exists 
+        // if it does not exist return error code and a message 
         return res.json({
           status: 409,
           message: 'The selected source has not been found.'
         });
       }
 
+      // taking the poperties of the wanted source
       const source = {};
       source.type = foundSource.type;
       source.url = foundSource.url;
@@ -139,6 +163,7 @@ router.post('/source',
       source.passcode = foundSource.passcode;
       source.vhost = foundSource.vhost;
     
+      // return the wanted source
       return res.json({
         success: true,
         source
