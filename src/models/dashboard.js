660fd4d1524d74196c123de64a50e4fa522ead69
Efthymios Grigorakis <73905568+eegrigor@users.noreply.github.com>
2023-01-19 22:16:42 +0200

    Comments Added

--- src/models/dashboard.js
+++ src/models/dashboard.js
@@ -7,42 +7,51 @@ mongoose.pluralize(null);
 
 const DashboardSchema = new mongoose.Schema(
   {
+    // The dashboard' name, it is always required
     name: {
       index: true,
       type: String,
       required: [true, 'Dashboard name is required']
     },
+    // The dashboard's layout
     layout: {
       type: Array,
       default: []
     },
+    // The items thah dashboard contains
     items: {
       type: Object,
       default: {}
     },
+    // The next id given
     nextId: {
       type: Number,
       min: 1,
       default: 1
     },
+    // If the dashboard has password and the password 
     password: {
       type: String,
       select: false,
       default: null
     },
+    // If the dashboard is shared with someone else 
     shared: {
       type: Boolean,
       default: false
     },
+    // How many views the dashboard has 
     views: {
       type: Number,
       default: 0,
       min: 0
     },
+    // The owner of the dashboard 
     owner: {
       type: mongoose.Schema.Types.ObjectId,
       ref: 'User'
     },
+    // Date that the dashboard was created 
     createdAt: {type: Date}
   }
 );
