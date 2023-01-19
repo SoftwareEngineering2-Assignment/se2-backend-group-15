45e828f856f31e988c16eae0a94df470fcf66e18
Efthymios Grigorakis <73905568+eegrigor@users.noreply.github.com>
2023-01-19 22:50:44 +0200

    Comments Added

--- src/middlewares/authorization.js
+++ src/middlewares/authorization.js
@@ -10,6 +10,7 @@ module.exports = (req, res, next) => {
     */
   pipe(
     (r) =>
+    // Take the token from one of paths 
       path(['query', 'token'], r)
           || path(['headers', 'x-access-token'], r)
           || path(['headers', 'authorization'], r),
@@ -22,25 +23,33 @@ module.exports = (req, res, next) => {
       isNil,
       () =>
         next({
+          // Checking if the token exists 
+          // If it does not exist return an error code and a message 
           message: 'Authorization Error: token missing.',
           status: 403
         }),
       (token) =>
+      // Checking if the token is correct 
         jwt.verify(token, secret, (e, d) =>
           ifElse(
             (err) => !isNil(err),
             (er) => {
+              // Checking if the token has expired 
               if (er.name === 'TokenExpiredError') {
+                // If it has expired return error code and a message 
                 next({
                   message: 'TokenExpiredError',
                   status: 401,
                 });
               }
+              // If the token is not correct 
+              // Return and error code and a message
               next({
                 message: 'Authorization Error: Failed to verify token.',
                 status: 403
               });
             },
+            // Decode the token 
             (_, decoded) => {
               req.decoded = decoded;
               return next();
