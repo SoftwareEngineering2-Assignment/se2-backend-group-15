441ad3f4fd9ea461ad1e79c3cdd04ed79653a7c5
Efthymios Grigorakis <73905568+eegrigor@users.noreply.github.com>
2023-01-19 22:24:31 +0200

    Comments Added

--- src/models/user.js
+++ src/models/user.js
@@ -6,8 +6,11 @@ const {constants: {min}} = require('../utilities/validation');
 
 mongoose.pluralize(null);
 
+// The structure of the user 
 const UserSchema = new mongoose.Schema(
   {
+    // The email of the user is reuired and in lowercase 
+    // It needs not to be in use from another user
     email: {
       index: true,
       type: String,
@@ -15,18 +18,23 @@ const UserSchema = new mongoose.Schema(
       required: [true, 'User email is required'],
       lowercase: true
     },
+    // The username of the user it is required 
+    // It needs not to be in use from another user
     username: {
       index: true,
       type: String,
       unique: 'A user already exists with that username!',
       required: [true, 'Username is required'],
     },
+    // A password for each user is required
+    // It must has a minimun length
     password: {
       type: String,
       required: [true, 'User password is required'],
       select: false,
       minlength: min
     },
+    // The date that user registered 
     registrationDate: {type: Number}
   }
 );
