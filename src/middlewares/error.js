8fcf33289ddde3cf977bdc49db1a6f57310df986
maitsidou <maryaitsidoy14@gmail.com>
2023-02-25 00:01:12 +0200

    Added comments

--- src/middlewares/error.js
+++ src/middlewares/error.js
@@ -1,8 +1,11 @@
 /* eslint-disable no-console */
 const {pipe, has, ifElse, assoc, identity, allPass, propEq} = require('ramda');
 
+// Every 500 status gets a modified message
 const withFormatMessageForProduction = ifElse(
   allPass([propEq('status', 500), () => process.env.NODE_ENV === 'production']),
+  
+  // Send server error message
   assoc('message', 'Internal server error occurred.'),
   identity
 );
@@ -14,7 +17,10 @@ module.exports = (error, res) =>
      */
   pipe(
     (e) => ({...e, message: e.message}),
+    // Give 500 status
     ifElse(has('status'), identity, assoc('status', 500)),
+    // Give the created message
     withFormatMessageForProduction,
+    // return error
     (fError) => res.status(fError.status).json(fError)
   )(error);
